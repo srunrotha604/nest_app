72a5133225dd021e1243e5aca5fc5262637ac1e8
rotha <srunrotha614@gmail.com>
2026-09-14 10:32:32 +0700

    new project

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,6 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [
-    // Distributed tracing, auto-correlated logs, request/job metrics, error
-    // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
     ObserveModule.forRoot({
       appKey: 'YOUR_APP_KEY',
       appSecret: 'YOUR_APP_SECRET',
