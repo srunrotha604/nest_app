acec06da72b214ac7c5fe8d13339ad369a1c71cd
rotha <srunrotha614@gmail.com>
2026-09-14 17:26:04 +0700

    config mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { AuthMiddleware } from './auth.middleware.js';
 import { CatsModule } from './cats.module.js';
 import { LoggerMiddleware } from './logger.middleware.js';
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 @Module({
-  imports: [ConfigModule.forRoot({}), CatsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest_user',
+      password: 'NestApp2026@#$',
+      database: 'nest_db',
+      entities: [],
+      synchronize: isDevelopment ? true : false,
+    }),
+    ConfigModule.forRoot({}),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
